b97492793d75cf425c4dddcf09e2c3e11cb31ea6
Sergejs Kozinecs <sergei.kozinecs@gmail.com>
2024-09-05 19:34:23 +0300

    Cleaned js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuBtn = document.getElementsByClassName("menu-btn");
-  const innerMenuBtn = document.getElementById("inner-menu-btn");
   const menuContent = document.getElementsByClassName("menu-content");
-  console.log(menuContent, menuBtn);
 
   for (let item of menuBtn) {
     item.addEventListener("click", () =>
